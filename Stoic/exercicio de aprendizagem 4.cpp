0bc9b98dafd624c1d1a98e914e09b17d4e985cdf
Jefferso Rodrigues <jefferson7810@gmail.com>
2023-07-28 21:47:56 -0300

    adição de exercicios de aprendizado

---
+++ Stoic/exercicio de aprendizagem 4.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+void calcular_volume();
+
+int raio, altura;
+double pi=3.14159;
+
+int main() 
+{
+	cout << "Calcula o volume de um cilindro" << endl;
+	cout << "--------------------------------" << endl;
+	cout << "Digite o raio do cilindro:" << endl;
+	cin >> raio;
+	cout << "Digite a altura do cilindro:" << endl;
+	cin >> altura;
+
+	calcular_volume();
+
+}
+
+void calcular_volume()
+{
+	float volume;
+
+	volume = (raio * raio) * pi * altura;
+	cout << " O volume do cilindro e: " << volume;
+}
